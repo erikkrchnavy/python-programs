b6bd2abb23f0752809a5f1ac1bebb4ce16db4834
erikkrchnavy <58730276+erikkrchnavy@users.noreply.github.com>
2019-12-10 12:01:28 +0100

    Create apples.py

---
+++ apples.py
@@ -0,0 +1,12 @@
+# apples.py
+
+# number of baskets
+baskets = 16
+
+# number of apples in a basket
+apples_in_basket = 24
+
+# we get the total number of apples
+total = baskets * apples_in_basket
+
+print(f"There are total of {total} apples")
